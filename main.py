7336b40915c5d690ed9dc4dc75327643be4977be
CiranoB <ciranobelardony@gmail.com>
2024-11-26 09:29:00 -0300

    Adicionando banco de Dados

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 from typing import Union
-
+from contextlib import asynccontextmanager
+import os
 from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-app = FastAPI()
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    age: int | None = Field(default=None, index=True)
+    secret_name: str
+
+DB_DRIVER = os.getenv("DB_DRIVER", default="postgresql")
+DB_USER = os.getenv("DB_USER", default="postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", default="postgres")
+DB_HOST = os.getenv("DB_HOST", default="postgres-fastapi")
+DB_PORT = os.getenv("DB_PORT", default="5432")
+DB_NAME = os.getenv("DB_NAME", default="postgres")
+
+
+connection_string = f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(connection_string)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
@@ -13,3 +41,17 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/heroes/")
+def create_hero(hero: Hero) -> Hero:
+    with Session(engine) as session:
+        session.add(hero)
+        session.commit()
+        session.refresh(hero)
+        return hero
+
+@app.get("/heroes/")
+def get_all_hero():
+    with Session(engine) as session:
+        stmt = select(Hero)
+        return session.exec(stmt).all()
